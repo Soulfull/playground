f7c76c68e64ac83ff50a13ef8bc4f735b9fb684a
Soulfull <mihailsmolin@gmail.com>
2019-09-11 18:05:47 +0300

    add console commit_1 rebase

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import appService from './services/appService.js';
 
-console.log('add commit 1');
+console.log('add commit 1_checkout');
 
 ReactDOM.render(<App appState={appService.appState} />, document.getElementById('root'));
 
